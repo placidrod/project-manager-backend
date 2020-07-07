f9119ba8233ebc7cf7eef0cb1323759a329da682
Placid Rodrigues <placidrod@gmail.com>
2020-07-07 19:24:49 +1200

    Remove unused vars

--- seeds/004-projects.js
+++ seeds/004-projects.js
@@ -1,5 +1,4 @@
 const tableName = 'projects'
-const { v4: uuidv4 } = require('uuid')
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
--- seeds/005-phases.js
+++ seeds/005-phases.js
@@ -1,5 +1,4 @@
 const tableName = 'phases'
-const { v4: uuidv4 } = require('uuid')
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
